bf4c10b6d26f10b01285030c5dfb2309e18e0fbd
GYC <873590539@qq.com>
2022-01-07 14:58:48 +0800

    2022年1月7日14:58:33

---
+++ src/main/java/com/eight/admin/EightPartEssayApplication.java
@@ -0,0 +1,13 @@
+package com.eight.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EightPartEssayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EightPartEssayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eight/admin/EightPartEssayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eight.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EightPartEssayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
